2add2f7bddc8e89b07be998932aa530fb9fd3723
lauanS <45686702+lauanS@users.noreply.github.com>
2019-09-17 12:13:17 -0300

    Assets e Scenes

---
+++ GDenfentory_proto/player.gd
@@ -0,0 +1,26 @@
+
+
+export var speed = 200
+var screen_size
+
+
+func _ready():
+	screen_size = get_viewport_rect().size
+	
+func _process(delta):
+	var velocity = Vector2()
+	
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	
+
+
