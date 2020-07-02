3d907b1bd3d37ef901537c5f6dfad734ccb67495
lauanS <lauan.s.souza@gmail.com>
2020-07-02 11:45:34 -0300

    Adicionando o projeto no Unity

---
+++ defentory_unity/Defentory/Assets/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public float panSpeed = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        if (Input.GetKey("w"))
+        {
+            pos.y += panSpeed * Time.deltaTime;
+        } 
+    }
+}
