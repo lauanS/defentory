428afb627fbc2944f1b5db536cd067347aed4edd
lauanS <lauan.s.souza@gmail.com>
2020-07-08 23:58:32 -0300

    Adicionado zoom e limites para câmera

--- defentory_unity/Defentory/Assets/Scripts/CameraController.cs
+++ defentory_unity/Defentory/Assets/Scripts/CameraController.cs
@@ -4,8 +4,15 @@ public class CameraController : MonoBehaviour
 {
     public float panSpeed = 10f;
     public float panBorderThickness = 20f;
+    public Vector2 panLimit;
+    
+    public float scrollSpeed = 100f;
+    public float smoothSpeed = 20f;
+    
+    public float minZoom = 4;
+    public float maxZoom = 8;
 
-    // Update is called once per frame
+    private bool borderMovement = false;
     void Update()
     {
         Vector3 pos = transform.position;
@@ -24,13 +31,25 @@ public class CameraController : MonoBehaviour
             pos.x += panSpeed * Time.deltaTime * horizontalCommand;
         } 
 
+        pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
+        pos.y = Mathf.Clamp(pos.y, -panLimit.y, panLimit.y);
         transform.position = pos;
+
+        float orthographicSize = GetComponent<Camera>().orthographicSize;
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        float zoomTarget = Mathf.Clamp(orthographicSize - scroll * scrollSpeed, minZoom, maxZoom);
+
+        if(scroll != 0){
+            GetComponent<Camera>().orthographicSize = Mathf.MoveTowards(orthographicSize, 
+                zoomTarget, smoothSpeed * Time.deltaTime);
+        }
+        
     }
 
     private float getVerticalCommand(){
         float verticalCommand = Input.GetAxis("Vertical");
 
-        if(verticalCommand == 0)
+        if(verticalCommand == 0 && borderMovement)
         {
             if(Input.mousePosition.y >= Screen.height - panBorderThickness)
             {
@@ -48,7 +67,7 @@ public class CameraController : MonoBehaviour
     private float getHorizontalCommand(){
         float horizontalCommand = Input.GetAxis("Horizontal");
 
-        if(horizontalCommand == 0)
+        if(horizontalCommand == 0  && borderMovement)
         {
             if(Input.mousePosition.x >= Screen.width - panBorderThickness)
             {
